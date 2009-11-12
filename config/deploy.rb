9f081b354e08b7bab5d21c5d9e634a9fd2922fbc
Craig Ambrose <craig@craigambrose.com>
2009-11-12 22:38:38 +1300

    added deploy config

---
+++ config/deploy.rb
@@ -0,0 +1,21 @@
+set :stages, %w(staging production)
+set :default_stage, "staging"
+
+require 'capistrano/ext/multistage'
+
+set :application, "living_models"
+set :repository,  "git@github.com:craigambrose/living_models.git"
+set :scm, "git"
+set :branch, "master" unless exists?(:branch)
+
+namespace :deploy do
+  desc "Restarting mod_rails with restart.txt"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+
+  [:start, :stop].each do |t|
+    desc "#{t} task is a no-op with mod_rails"
+    task t, :roles => :app do ; end
+  end
+end
