a840c2948e827dd11fbcf87a2a483c735c52e244
Craig Ambrose <craig@craigambrose.com>
2009-12-17 18:30:49 +1100

    Started styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,11 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def site_section_link(id, text, path, options = {})
+    classes = ['site_section']
+    classes << 'active' if id == @site_section
+    options[:class] = [options[:class], classes].flatten.compact.join(' ')
+    options[:id] = id
+    link_to(content_tag(:span, text), path, options)
+  end    
+
 end
