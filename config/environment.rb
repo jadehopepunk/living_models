a3d5ecc6dc9d7b4d6fbc4652dc261676e5f6321f
Craig Ambrose <craig@craigambrose.com>
2010-01-09 20:32:00 +1300

    changed inherited resources to a gem

--- config/environment.rb
+++ config/environment.rb
@@ -15,10 +15,11 @@ Rails::Initializer.run do |config|
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
 
   # Specify gems that this application depends on and have them installed with rake gems:install
-  config.gem "rspec", :lib => false, :version => ">= 1.2.9"
-  config.gem "rspec-rails", :lib => false, :version => ">= 1.2.9"
-  config.gem "haml", :lib => false, :version => ">= 2.2.13"
-  config.gem 'paperclip', :source => 'http://gemcutter.org'
+  config.gem "rspec", :lib => false, :version => "1.2.9"
+  config.gem "rspec-rails", :lib => false, :version => "1.2.9"
+  config.gem "haml", :lib => false, :version => "2.2.16"
+  config.gem 'paperclip', :source => 'http://gemcutter.org', :version => "2.3.1.1"
+  config.gem 'inherited_resources', :version => "1.0.1"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :projects
+  map.resources :projects, :has_many => :photos
+  map.resources :photos
   map.root :controller => "projects"
 end
