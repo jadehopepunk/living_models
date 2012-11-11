4ab462866134b0bdcb563e9e423d7e485a529dfe
Craig Ambrose <craig@craigambrose.com>
2012-11-12 07:55:25 +1300

    started rails3 upgrade

--- Gemfile
+++ Gemfile
@@ -1,13 +1,13 @@
 source :rubygems
 
-gem 'rails', '2.3.11'
-gem 'mysql'
-gem "haml", "2.2.16"
+gem 'rails', '3.0.8'
+gem 'mysql2'
+gem "haml"
 gem 'paperclip', "2.5.0"
 gem 'inherited_resources', "1.0.0"
 gem 'responders', '0.4.0'
-gem 'authlogic', "2.1.3"
-gem 'will_paginate', '2.3.11'
+gem 'authlogic'
+gem 'will_paginate'
 gem 'hoptoad_notifier', '2.3.5'
 gem 'formtastic', '1.2.4'
 
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RealSolutions::Application.config.secret_token = '23d0e974e31dc3904e6d755e27e54429e03c8347849c4dc26e0721c5ca0527640a03a70bb496e74e62ee1733b084c790c74515a520ffe2637d04a4b38464af5e'
