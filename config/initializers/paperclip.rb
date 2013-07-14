97317ecaeaaa2ba695cd1919f8f56a3118b9dda9
Craig Ambrose <craig@craigambrose.com>
2013-07-15 11:02:39 +1200

    better path changes

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,3 +1,1 @@
-Paperclip.options[:command_path] = ENV['IMAGEMAGICK_ROOT']
-Paperclip.options[:command_path] = %x[which identify].strip.gsub(/\/identify$/, '') if Paperclip.options[:command_path].blank? 
-Paperclip.options[:command_path] = '/usr/bin/identify' if Paperclip.options[:command_path].blank?
+Paperclip.options[:command_path] = %x[which identify].strip.gsub(/\/identify$/, '')
